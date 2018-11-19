2f1a4d034b5fb0bce136f940deaadd336b785005
AdityaT12 <adithya.tripathi@rsrit.com>
2018-11-19 11:29:54 -0500

    Navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() { 
+        return (<nav class="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+      </nav>  );
+    }
+}
+ 
+export default Navbar;
